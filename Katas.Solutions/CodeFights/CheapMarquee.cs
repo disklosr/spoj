fa740aaf5fc184d6b6cf1c70ea6b13d73339006d
Anas Mazioudi <disklosr@outlook.com>
2018-04-28 21:15:53 +0200

    golf

--- Katas.Solutions/CodeFights/CheapMarquee.cs
+++ Katas.Solutions/CodeFights/CheapMarquee.cs
@@ -8,20 +8,20 @@ namespace Katas.Solutions.CodeFights
     {
         int Solve(string m, int w)
         {
-            if (w == 0) return 0;
+            
+
 
             var u = new object[]
                 {
                     "00", 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 3443, 2511, 2332, 3332, 2251, 4332, 3332, 2222,
                     2332, 2333, 0, 0, 0, 0, 0, 1321, 0, 4224, 5332, 3222, 5223, 5332, 5221, 3233, 5115, 2522, 2241,
                     5122, 5111, 5225, 5125, 3223, 5221, 3234, 5332, 2332, 1511, 4114, 4122, 4225, 3223, 2311, 3332
-                }.Select(_ => _.ToString()).ToArray();
+                }.Select(_ => _+"").ToArray();
 
-            m = m.PadRight(Math.Max(m.Length, w)).ToUpper().Select(_ => _ - ' ').Where(c => c < 59 && u[c] != "0")
+            m = m.PadRight(99).ToUpper().Select(_ => _ - ' ').Where(c => c < 59 && u[c] != "0")
                 .Aggregate("", (x, c) => x + (u[c] + "0"));
 
-            return Enumerable.Range(0, m.Length - w - 1).Select(i => m.Skip(i).Take(w).Select(c => c - '0').Sum())
-                .Max();
+            return new int[9999].Select((_, i) => m.Skip(i).Take(w).Select(c => c - '0').Sum()).Max();
         }
 
         [Test]
